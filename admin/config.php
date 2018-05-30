f72cb0a6c0d93454adb85ea7dd1f2d11bd7abc56
Makes-MS <MakesMS24@gmail.com>
2018-05-30 10:40:33 +0300

    Install OpenCart v0

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/stylyaga/admin/');
+define('HTTP_CATALOG', 'http://localhost/stylyaga/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/stylyaga/admin/');
+define('HTTPS_CATALOG', 'http://localhost/stylyaga/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/stylyaga/admin/');
+define('DIR_SYSTEM', '/var/www/html/stylyaga/system/');
+define('DIR_IMAGE', '/var/www/html/stylyaga/image/');
+define('DIR_LANGUAGE', '/var/www/html/stylyaga/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/stylyaga/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/stylyaga/system/config/');
+define('DIR_CACHE', '/var/www/html/stylyaga/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/stylyaga/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/stylyaga/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/stylyaga/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/stylyaga/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/stylyaga/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '7422');
+define('DB_DATABASE', 'stylyaga_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/stylyaga/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/stylyaga/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/stylyaga/catalog/');
+define('DIR_SYSTEM', '/var/www/html/stylyaga/system/');
+define('DIR_IMAGE', '/var/www/html/stylyaga/image/');
+define('DIR_LANGUAGE', '/var/www/html/stylyaga/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/stylyaga/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/stylyaga/system/config/');
+define('DIR_CACHE', '/var/www/html/stylyaga/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/stylyaga/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/stylyaga/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/stylyaga/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/stylyaga/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '7422');
+define('DB_DATABASE', 'stylyaga_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
